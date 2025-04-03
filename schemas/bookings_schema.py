4863d4de25dcb0c006d495f92f2bb2170e2a4aca
CyberMan <56507330+Muammer80@users.noreply.github.com>
2025-04-04 00:34:27 +0200

    create bookings_schema

--- schemas/bookings_schema.py
+++ schemas/bookings_schema.py
@@ -1,7 +1,30 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional , List
+
+from schemas.users_schema import UserDisplay
+from schemas.vehicles_schema import VehicleDisplay
 
 class BookingBase(BaseModel):
-    vehicle_id: int
+    rented_vehicle_id: int
     start_time: datetime
     end_time: datetime
+
+class BookingDisplay(BaseModel):
+    booking_id: int
+    booking_date: datetime
+    start_time: datetime
+    end_time: datetime
+    total_days: int
+
+    is_cancelled: Optional[bool] = None
+    cancelled_at: Optional[datetime] = None
+    cancellation_type: Optional[str] = None
+
+    is_delivered_up: Optional[bool] = None
+    damage_report: Optional[str] = None
+    is_report_approved: Optional[bool] = None
+    class Config:
+        from_attributes = True
+    
+    
