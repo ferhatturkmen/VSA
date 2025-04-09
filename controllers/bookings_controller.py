3c4cde82021b3c46f2aed29a36210df34176e351
ferhatturkmen <ferhatturkmen@outlook.com>
2025-04-10 00:43:03 +0200

    files&booking exceptions and status codes added

--- controllers/bookings_controller.py
+++ controllers/bookings_controller.py
@@ -54,92 +54,127 @@ def create_booking(db: Session,
 
 def get_all_bookings(db: Session,
                      query_params: Optional[BookingQuery] ):
-    req_db_query = db.query(db_booking)
-    if query_params:
-        if query_params.booking_id:
-            req_db_query = req_db_query.filter(db_booking.booking_id == query_params.booking_id)
-        if query_params.rented_vehicle_id:
-            req_db_query = req_db_query.filter(db_booking.rented_vehicle_id == query_params.rented_vehicle_id)
-        if query_params.renter_id:
-            req_db_query = req_db_query.filter(db_booking.renter_id == query_params.renter_id)
-        if query_params.start_date:
-            req_db_query = req_db_query.filter(db_booking.start_date == query_params.start_date)
-        if query_params.end_date:
-            req_db_query = req_db_query.filter(db_booking.end_date == query_params.end_date)
-        if query_params.created_at:
-            req_db_query = req_db_query.filter(db_booking.created_at == query_params.created_at)
-        if query_params.is_delivered_up is not None:
-            req_db_query = req_db_query.filter(db_booking.is_delivered_up == query_params.is_delivered_up)
-        if query_params.damage_report:
-            req_db_query = req_db_query.filter(db_booking.damage_report == query_params.damage_report)
-        if query_params.is_report_approved is not None:
-            req_db_query = req_db_query.filter(db_booking.is_report_approved == query_params.is_report_approved)
-        if query_params.approved_at:
-            req_db_query = req_db_query.filter(db_booking.approved_at == query_params.approved_at)
-        if query_params.is_cancelled is not None:
-            req_db_query = req_db_query.filter(db_booking.is_cancelled == query_params.is_cancelled)
-        if query_params.cancelled_at:
-            req_db_query = req_db_query.filter(db_booking.cancelled_at == query_params.cancelled_at)
+    try:
+        req_db_query = db.query(db_booking)
+        if query_params:
+            if query_params.booking_id:
+                req_db_query = req_db_query.filter(db_booking.booking_id == query_params.booking_id)
+            if query_params.rented_vehicle_id:
+                req_db_query = req_db_query.filter(db_booking.rented_vehicle_id == query_params.rented_vehicle_id)
+            if query_params.renter_id:
+                req_db_query = req_db_query.filter(db_booking.renter_id == query_params.renter_id)
+            if query_params.start_date:
+                req_db_query = req_db_query.filter(db_booking.start_date == query_params.start_date)
+            if query_params.end_date:
+                req_db_query = req_db_query.filter(db_booking.end_date == query_params.end_date)
+            if query_params.created_at:
+                req_db_query = req_db_query.filter(db_booking.created_at == query_params.created_at)
+            if query_params.is_delivered_up is not None:
+                req_db_query = req_db_query.filter(db_booking.is_delivered_up == query_params.is_delivered_up)
+            if query_params.damage_report:
+                req_db_query = req_db_query.filter(db_booking.damage_report == query_params.damage_report)
+            if query_params.is_report_approved is not None:
+                req_db_query = req_db_query.filter(db_booking.is_report_approved == query_params.is_report_approved)
+            if query_params.approved_at:
+                req_db_query = req_db_query.filter(db_booking.approved_at == query_params.approved_at)
+            if query_params.is_cancelled is not None:
+                req_db_query = req_db_query.filter(db_booking.is_cancelled == query_params.is_cancelled)
+            if query_params.cancelled_at:
+                req_db_query = req_db_query.filter(db_booking.cancelled_at == query_params.cancelled_at)
+        
+        return req_db_query.all()
     
-    return req_db_query.all()
+    except HTTPException as e:
+        raise e
+    except Exception as e:        
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Exception occured while getting bookings"
+        )
 
 
 # Read a booking 
 
 def get_booking(db: Session, booking_id: int, current_user):
-    req_booking = db.query(db_booking).filter(db_booking.booking_id == booking_id, db_booking.renter_id == current_user.user_id).first()
-    if not req_booking:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f'Requested booking with id {booking_id} is not found')
-    return req_booking
+    try:
+        req_booking = db.query(db_booking).filter(db_booking.booking_id == booking_id, db_booking.renter_id == current_user.user_id).first()
+        if not req_booking:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail=f'Requested booking with id {booking_id} is not found')
+        return req_booking
+    except HTTPException as e:
+        raise e
+    except Exception as e:      
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Exception occured while getting booking"
+        )
 
 # Update Booking
 
 def update_booking(db: Session, booking_id: int, request: BookingQuery):
-    req_booking = db.query(db_booking).filter(db_booking.booking_id == booking_id).first()
-    if not req_booking:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Requested booking with id {booking_id} is not found.")
-    if request.start_date <= datetime.now(pytz.timezone('Europe/Amsterdam')):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Cannot edit a booking that has already started")
+    try:
+        req_booking = db.query(db_booking).filter(db_booking.booking_id == booking_id).first()
+        if not req_booking:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail=f"Requested booking with id {booking_id} is not found.")
+        if request.start_date <= datetime.now(pytz.timezone('Europe/Amsterdam')):
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Cannot edit a booking that has already started")
+        
+        if request.booking_id is not None:
+            req_booking.booking_id = request.booking_id
+        if request.rented_vehicle_id is not None:
+            req_booking.rented_vehicle_id = request.rented_vehicle_id
+        if request.renter_id is not None:
+            req_booking.renter = request.renter_id
+        if request.start_date is not None:
+            req_booking.start_date = request.start_date
+        if request.end_date is not None:
+            req_booking.end_date = request.end_date
+        if request.is_delivered_up is not None:
+            req_booking.is_delivered_up = request.is_delivered_up
+        if request.damage_report is not None:
+            req_booking.damage_report = request.damage_report
+        if request.is_report_approved is not None:
+            req_booking.is_report_approved = request.is_report_approved
+        if request.approved_at is not None:
+            req_booking.approved_at = request.approved_at
+        if request.is_cancelled is not None:
+            req_booking.is_cancelled = request.is_cancelled
+        if request.cancelled_at is not None:
+            req_booking.cancelled_at = request.cancelled_at
+        
+        db.commit()
+        db.refresh(req_booking)
+        return req_booking
     
-    if request.booking_id is not None:
-        req_booking.booking_id = request.booking_id
-    if request.rented_vehicle_id is not None:
-        req_booking.rented_vehicle_id = request.rented_vehicle_id
-    if request.renter_id is not None:
-        req_booking.renter = request.renter_id
-    if request.start_date is not None:
-        req_booking.start_date = request.start_date
-    if request.end_date is not None:
-        req_booking.end_date = request.end_date
-    if request.is_delivered_up is not None:
-        req_booking.is_delivered_up = request.is_delivered_up
-    if request.damage_report is not None:
-        req_booking.damage_report = request.damage_report
-    if request.is_report_approved is not None:
-        req_booking.is_report_approved = request.is_report_approved
-    if request.approved_at is not None:
-        req_booking.approved_at = request.approved_at
-    if request.is_cancelled is not None:
-        req_booking.is_cancelled = request.is_cancelled
-    if request.cancelled_at is not None:
-        req_booking.cancelled_at = request.cancelled_at
-      
-    db.commit()
-    db.refresh(req_booking)
-    return req_booking
+    except HTTPException as e:
+        raise e
+    except Exception as e:        
+        db.rollback() 
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Exception occured while updating booking"
+        )
 
 #Delete a booking
 def delete_booking(db: Session, booking_id: int, ):
-    req_booking = db.query(db_booking).filter(db_booking.booking_id == booking_id).first()
-    if not req_booking:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Requested booking with id {booking_id} is not found.")
-    db.delete(req_booking)
-    db.commit()
-    return f"Requested booking with id {booking_id} deleted."
+    try:
+        req_booking = db.query(db_booking).filter(db_booking.booking_id == booking_id).first()
+        if not req_booking:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail=f"Requested booking with id {booking_id} is not found.")
+        db.delete(req_booking)
+        db.commit()
+        return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
+    except HTTPException as e:
+        raise e
+    except Exception as e:   
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Exception occured while deleting booking"
+        )
 
 
 
--- controllers/files_controller.py
+++ controllers/files_controller.py
@@ -23,58 +23,68 @@ def allowed_file(filename: str) -> bool:
     return extension in ALLOWED_EXTENSIONS
 
 def upload_vehicle_file(db: Session, vehicle_id: int, files: list[UploadFile]):
-    req_vehicle = db.query(db_vehicle).filter(db_vehicle.vehicle_id == vehicle_id).first()
-    if not req_vehicle:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-                            detail=f"Vehicle with id {vehicle_id} not found")
+    try:
+        req_vehicle = db.query(db_vehicle).filter(db_vehicle.vehicle_id == vehicle_id).first()
+        if not req_vehicle:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                                detail=f"Vehicle with id {vehicle_id} not found")
 
-    file_paths = []     
-    # Process each file in the 'files' list
-    for file in files:
-        if not allowed_file(file.filename):
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail=f"File type {file.filename} is not allowed. Allowed extensions are: {', '.join(ALLOWED_EXTENSIONS)}"
-            )
+        file_paths = []     
+        # Process each file in the 'files' list
+        for file in files:
+            if not allowed_file(file.filename):
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail=f"File type {file.filename} is not allowed. Allowed extensions are: {', '.join(ALLOWED_EXTENSIONS)}"
+                )
+            
+            # Check file size (10 MB limit)
+            file_size = len(file.file.read()) 
+            file.file.seek(0) 
+            if file_size > MAX_FILE_SIZE:
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail=f"File {file.filename} is too large. Maximum allowed size is 10 MB."
+                )
+
+            if not os.path.exists(UPLOAD_FOLDER):
+                os.makedirs(UPLOAD_FOLDER)
+
+            sanitized_filename = file.filename.replace(" ", "_")
+            file_location = os.path.join(UPLOAD_FOLDER, sanitized_filename)
+
+            # file saving to the specified location
+            with open(file_location, "wb") as f:
+                f.write(file.file.read())
+
+            # file adding entry to the database
+            add_file = db_vehicle_files(vehicle_id=vehicle_id, file_url=file_location)
+            db.add(add_file)
+            db.commit()
+            db.refresh(add_file)
+
+            # Append the file location to the file_paths list
+            file_paths.append(file_location)
+
+
+        # Return a response after all files are processed
+        return JSONResponse(
+            status_code=201,
+            content={
+                "message": "File uploaded successfully!",
+                "file_paths": file_paths
+            }
+        )
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        # Rollback the transaction in case of error
+        db.rollback() 
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Exception occured while uploading files"
+        )
         
-        # Check file size (10 MB limit)
-        file_size = len(file.file.read()) 
-        file.file.seek(0) 
-        if file_size > MAX_FILE_SIZE:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail=f"File {file.filename} is too large. Maximum allowed size is 10 MB."
-            )
-
-        if not os.path.exists(UPLOAD_FOLDER):
-            os.makedirs(UPLOAD_FOLDER)
-
-        sanitized_filename = file.filename.replace(" ", "_")
-        file_location = os.path.join(UPLOAD_FOLDER, sanitized_filename)
-
-        # file saving to the specified location
-        with open(file_location, "wb") as f:
-            f.write(file.file.read())
-
-        # file adding entry to the database
-        add_file = db_vehicle_files(vehicle_id=vehicle_id, file_url=file_location)
-        db.add(add_file)
-        db.commit()
-        db.refresh(add_file)
-
-        # Append the file location to the file_paths list
-        file_paths.append(file_location)
-
-
-    # Return a response after all files are processed
-    return JSONResponse(
-        status_code=201,
-        content={
-            "message": "File uploaded successfully!",
-            "file_paths": file_paths
-        }
-    )
-    
     # file_paths = []   
     # for file in files:  
               
@@ -105,16 +115,24 @@ def upload_vehicle_file(db: Session, vehicle_id: int, files: list[UploadFile]):
 
 
 def get_files_by_car(db: Session, vehicle_id: int):
-        
-    car = db.query(db_vehicle).filter(db_vehicle.vehicle_id == vehicle_id).first()
-    if not car:
-        raise HTTPException(status_code=404, detail=f"Vehicle with id {vehicle_id} not found")
+    try:    
+        car = db.query(db_vehicle).filter(db_vehicle.vehicle_id == vehicle_id).first()
+        if not car:
+            raise HTTPException(status_code=404, detail=f"Vehicle with id {vehicle_id} not found")
 
-    images = db.query(db_vehicle_files).filter(db_vehicle_files.vehicle_id == vehicle_id).all()
-    if not images:
-        raise HTTPException(status_code=404, detail=f"No files found for vehicle with id {vehicle_id}")
+        images = db.query(db_vehicle_files).filter(db_vehicle_files.vehicle_id == vehicle_id).all()
+        if not images:
+            raise HTTPException(status_code=404, detail=f"No files found for vehicle with id {vehicle_id}")
 
-    return images
+        return 
+    
+    except HTTPException as e:
+        raise e
+    except Exception as e: 
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Exception occured while getting image"
+        )
 
 
 
@@ -125,14 +143,22 @@ def get_files_by_car(db: Session, vehicle_id: int):
 
 
 def delete_file(db: Session, file_id:int):
-   file_to_delete = db.query(db_vehicle_files).filter(db_vehicle_files.file_id == file_id).first()
-   if not file_to_delete:
-       raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                           detail=f"File with id {file_id} not found")
-       
-   db.delete(file_to_delete)
-   db.commit()
-   return Response(status_code=status.HTTP_204_NO_CONTENT)
+    try:
+        file_to_delete = db.query(db_vehicle_files).filter(db_vehicle_files.file_id == file_id).first()
+        if not file_to_delete:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail=f"File with id {file_id} not found")
+            
+        db.delete(file_to_delete)
+        db.commit()
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    except HTTPException as e:
+        raise e
+    except Exception as e:        
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Exception occured while deleting image"
+        )
 
 
 
