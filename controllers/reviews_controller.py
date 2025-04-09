10fab793672bc2d6ec1cf3d818487e1d8ee7f117
ferhatturkmen <ferhatturkmen@outlook.com>
2025-04-10 00:58:19 +0200

    payment&review exceptions and status codes added

--- controllers/reviews_controller.py
+++ controllers/reviews_controller.py
@@ -35,51 +35,86 @@ def create_review(db:Session, request: ReviewBase):
     
 
 def get_review(db:Session, review_id:int):
-      req_review = db.query(db_review).filter(db_review.review_id == review_id).first()
-      if not req_review:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-        detail=f'Requested review with id {review_id} is not found')
-      return req_review
+    try:
+        req_review = db.query(db_review).filter(db_review.review_id == review_id).first()
+        if not req_review:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                detail=f'Requested review with id {review_id} is not found')
+        return req_review
+    
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Exception occured while getting review"
+        )
   
 def get_all_review(db: Session, query_params: Optional[ReviewQuery]):
-  req_db_query = db.query(db_review)
-  if query_params:
-    if query_params.review_id:
-      req_db_query = req_db_query.filter(db_review.review_id == query_params.review_id)
-    if query_params.booking_id:
-        req_db_query = req_db_query.filter(db_review.booking_id == query_params.booking_id)
-    if query_params.review_type:
-        req_db_query = req_db_query.filter(db_review.review_type == query_params.review_type)
-    if query_params.review_rating:
-        req_db_query = req_db_query.filter(db_review.review_rating == query_params.review_rating)
-  return req_db_query.all()
+    try:
+        req_db_query = db.query(db_review)
+        if query_params:
+            if query_params.review_id:
+                req_db_query = req_db_query.filter(db_review.review_id == query_params.review_id)
+            if query_params.booking_id:
+                req_db_query = req_db_query.filter(db_review.booking_id == query_params.booking_id)
+            if query_params.review_type:
+                req_db_query = req_db_query.filter(db_review.review_type == query_params.review_type)
+            if query_params.review_rating:
+                req_db_query = req_db_query.filter(db_review.review_rating == query_params.review_rating)
+        return req_db_query.all()
+    
+    except HTTPException as e:
+        raise e
+    except Exception as e:        
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Exception occured while getting"
+        )
 
 def update_review(db:Session, review_id:int, request:ReviewQuery):
-     req_review= db.query(db_review).filter(db_review.review_id== review_id).first()
-     if not req_review:
+    try:
+        req_review= db.query(db_review).filter(db_review.review_id== review_id).first()
+        if not req_review:        
+            HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Requested review with id {review_id} is not found')
         
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-        detail=f'Requested review with id {review_id} is not found')
-     else:
         if request.booking_id is not None:
             req_review.booking_id = request.booking_id
         if request.review_type is not None:
             req_review.review_type = request.review_type
         if request.review_rating is not None:
             req_review.review_rating =request.review_rating 
-     db.commit()
-     db.refresh(req_review)
-     return req_review
+        db.commit()
+        db.refresh(req_review)
+        return req_review
+    except HTTPException as e:
+        raise e
+    except Exception as e:        
+        db.rollback() 
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Exception occured while updating review"
+        )
 
 def delete_review(db:Session, review_id:int):
-    req_review = db.query(db_review).filter(db_review.review_id == review_id).first()
-    if not req_review:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-        detail=f'Requested review with id {review_id} is not found')
-    else:
-        db.delete(req_review)
-        db.commit()
-    return f"Review with id {review_id} is deleted"
+    try:
+        req_review = db.query(db_review).filter(db_review.review_id == review_id).first()
+        if not req_review:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Requested review with id {review_id} is not found')
+        else:
+            db.delete(req_review)
+            db.commit()
+        return JSONResponse(status_code=status.HTTP_204_NO_CONTENT) 
+    
+    except HTTPException as e:
+        raise e
+    except Exception as e:        
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Exception occured while deleting review"
+        )
 
 
 #reviews_contrpoller.py
